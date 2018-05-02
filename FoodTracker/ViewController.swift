7b78fc16c09fe35e5863e632aeacc4a22ae49cc0
Caston Boyd <castonboyd@gmail.com>
2018-05-02 16:19:44 -0400

    More Code to VC

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,12 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     //MARK: - Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     
+    @IBOutlet weak var photoImage: UIImageView!
     
     
 
@@ -35,11 +36,41 @@ class ViewController: UIViewController, UITextFieldDelegate {
         mealNameLabel.text = textField.text
     }
     
+    //MARK: - UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else{
+            fatalError("Expected a dictionary containing an image, but was provided with the following \(info)")
+            
+            
+        }
+        photoImage.image = selectedImage
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    
     //MARK: - Actions
+    
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        //this action hides the keyboard
+        nameTextField.resignFirstResponder()
+        
+        //this assign a constant type to an instance of UIImagePicker
+        let imagePicker = UIImagePickerController()
+       imagePicker.sourceType = .photoLibrary
+       imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+        
+        
+    }
     
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         mealNameLabel.text = "Default Text"
     }
-    
+   
 }
 
